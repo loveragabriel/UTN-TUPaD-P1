6f187e53ff7c9d4c323ef9c24093ffa382f68bcc
gabriel <loveragabriel20@gmail.com>
2025-06-19 16:59:17 -0300

    update actividad 8

--- 06 Datos complejos/LOVERA_TP-06_1.py
+++ 06 Datos complejos/LOVERA_TP-06_1.py
@@ -46,7 +46,7 @@ print(listado_de_frutas)
 4) Escribí un programa que permita almacenar y consultar números telefónicos.
 • Permití al usuario cargar 5 contactos con su nombre como clave y número como valor.
 • Luego, pedí un nombre y mostrale el número asociado, si existe.
-'''
+
 
 diccionario_telefonico = {}
 
@@ -56,11 +56,99 @@ for i in range(5):
     diccionario_telefonico[nombre]= numero
 
 def preguntar_nombre(nombre, diccionario):
-    nombre = input
     if nombre in diccionario:
         print(f"El numero para {nombre} es {diccionario[nombre]}")
     else: print(f"No hay registro para el nombre {nombre}")
 
-nombre = input("Ingrese un nombre para buscar su numero")
+nombre = input("Ingrese un nombre para buscar su numero ")
 preguntar_nombre(nombre, diccionario_telefonico)
-            
+
+5) Solicita al usuario una frase e imprime:
+• Las palabras únicas (usando un set).
+• Un diccionario con la cantidad de veces que aparece cada palabra.
+
+frase = input("Ingrese una frase: ")
+diccionario_frase = frase.split()
+palabras_unicas = set(diccionario_frase)
+print(f"Estas son las palabras únicas: {palabras_unicas}")
+print("Repeticiones de cada palabra:")
+    
+contador_palabras= {}
+for palabra in palabras_unicas:
+    cantidad = diccionario_frase.count(palabra)
+    contador_palabras[palabra] = cantidad
+print(contador_palabras)
+
+6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas.
+Luego, mostrá el promedio de cada alumno.
+
+
+diccionario_alumnos = {}
+
+for i in range(3):
+    nombre_alumno = input("Ingrese nombre del alumno: ")
+    notas_alumon = []
+    for n in range(3):
+        nota = float(input(f"Nota {n+1} para {nombre_alumno}: "))
+        notas_alumon.append(nota)
+    diccionario_alumnos[nombre_alumno] = tuple(notas_alumon)
+
+print(diccionario_alumnos)
+
+7) Dado dos sets de números, representando dos listas de estudiantes que aprobaron Parcial 1
+y Parcial 2:
+• Mostrá los que aprobaron ambos parciales.
+• Mostrá los que aprobaron solo uno de los dos.
+• Mostrá la lista total de estudiantes que aprobaron al menos un parcial (sin repetir).
+
+
+parcial_1 = {("Ana", 8), ("Luis", 5), ("Juan", 9)}
+parcial_2 = {("Ana", 7), ("Luis", 6), ("Marta", 10)}
+
+aprobados_1={} 
+for nombre, nota in parcial_1:
+    if nota >= 6:
+        aprobados_1[nombre] = nota
+
+aprobados_2 = {}
+for nombre, nota in parcial_2:
+    if nota >= 6:
+        aprobados_2[nombre] = nota
+
+aprobados_ambos_parciales = set(aprobados_1) & set(aprobados_2)
+aprobado_solo_1 = solo_uno = set(aprobados_1) ^ set(aprobados_2)
+aprobados_al_menos_1 = al_menos_uno = set(aprobados_1) | set(aprobados_2)
+
+print(f"Aprobaron ambos: {aprobados_ambos_parciales}")
+print(f"Aprobaron solo uno: {aprobado_solo_1}")
+print(f"Aprobaron al menos uno: {aprobados_al_menos_1}")
+
+
+8) Armá un diccionario donde las claves sean nombres de productos y los valores su stock.
+Permití al usuario:
+• Consultar el stock de un producto ingresado.
+• Agregar unidades al stock si el producto ya existe.
+• Agregar un nuevo producto si no existe.
+'''
+
+stock_productos = {
+    "pan": 20,
+    "leche": 6,
+    "huevos": 20, 
+    "azucar": 6
+}
+
+productos_ingresado = stock_productos.keys()
+print(f"El esto de productos es {productos_ingresado}")
+
+producto_agregar = input("Indique un productos para agregar unidades ").lower()
+
+if producto_agregar in stock_productos:
+    cantidad = int(input("¿Cuántas unidades querés agregar?: "))
+    stock_productos[producto_agregar] += cantidad
+    print(f"Nuevo stock de '{producto_agregar}': {stock_productos[producto_agregar]} unidades.")
+else:
+    print(f"El producto '{producto_agregar}' no existe en el stock, pero puede ingresarlo.")
+    cantidad = int(input("¿Cuántas unidades tiene el nuevo producto?: "))
+    stock_productos[producto_agregar] = cantidad
+    print(f"Producto '{producto_agregar}' agregado con {cantidad} unidades.")
