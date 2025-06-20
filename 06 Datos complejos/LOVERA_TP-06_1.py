b41e0ec65be5126dce206d51eabde2d5276ba03b
Gabriel <loveragabriel20@gmail.com>
2025-06-20 00:44:00 -0300

    Practica Datos complejos Nunidad 6

--- 06 Datos complejos/LOVERA_TP-06_1.py
+++ 06 Datos complejos/LOVERA_TP-06_1.py
@@ -2,9 +2,9 @@
 Estructura de Datos Complejas
 Gabriel Lovera
 '''
-'''
 
-####################################Actividades#################################33
+####################################Actividades#################################
+'''
 1) Dado el diccionario precios_frutas
 precios_frutas = {'Banana': 1200, 'Ananá': 2500, 'Melón': 3000, 'Uva':
 1450}
@@ -22,31 +22,35 @@ precios_frutas['Pera'] = 2300
 
 print(f"Listado de Frutas actualizadas: {precios_frutas}")
 
+
 '''
 2) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
 desarrollado en el punto anterior, actualizar los precios de las siguientes frutas:
 ● Banana = 1330
 ● Manzana = 1700
 ● Melón = 2800
-
+'''
 precios_frutas['Banana'] = 1330
 precios_frutas['Manzana'] = 1700
 precios_frutas['Melón'] = 2800
 
 print(F"Precio de Frutas actualizados: {precios_frutas}")
-'''
+
+
 '''
 3) Siguiendo con el diccionario precios_frutas que resulta luego de ejecutar el código
 desarrollado en el punto anterior, crear una lista que contenga únicamente las frutas sin los
 precios.
+'''
 listado_de_frutas = precios_frutas.keys()
 print(listado_de_frutas)
-'''
+
+
 '''
 4) Escribí un programa que permita almacenar y consultar números telefónicos.
 • Permití al usuario cargar 5 contactos con su nombre como clave y número como valor.
 • Luego, pedí un nombre y mostrale el número asociado, si existe.
-
+'''
 
 diccionario_telefonico = {}
 
@@ -63,10 +67,12 @@ def preguntar_nombre(nombre, diccionario):
 nombre = input("Ingrese un nombre para buscar su numero ")
 preguntar_nombre(nombre, diccionario_telefonico)
 
+
+'''
 5) Solicita al usuario una frase e imprime:
 • Las palabras únicas (usando un set).
 • Un diccionario con la cantidad de veces que aparece cada palabra.
-
+'''
 frase = input("Ingrese una frase: ")
 diccionario_frase = frase.split()
 palabras_unicas = set(diccionario_frase)
@@ -79,9 +85,11 @@ for palabra in palabras_unicas:
     contador_palabras[palabra] = cantidad
 print(contador_palabras)
 
+
+'''
 6) Permití ingresar los nombres de 3 alumnos, y para cada uno una tupla de 3 notas.
 Luego, mostrá el promedio de cada alumno.
-
+'''
 
 diccionario_alumnos = {}
 
@@ -95,12 +103,14 @@ for i in range(3):
 
 print(diccionario_alumnos)
 
+
+'''
 7) Dado dos sets de números, representando dos listas de estudiantes que aprobaron Parcial 1
 y Parcial 2:
 • Mostrá los que aprobaron ambos parciales.
 • Mostrá los que aprobaron solo uno de los dos.
 • Mostrá la lista total de estudiantes que aprobaron al menos un parcial (sin repetir).
-
+'''
 
 parcial_1 = {("Ana", 8), ("Luis", 5), ("Juan", 9)}
 parcial_2 = {("Ana", 7), ("Luis", 6), ("Marta", 10)}
@@ -124,6 +134,7 @@ print(f"Aprobaron solo uno: {aprobado_solo_1}")
 print(f"Aprobaron al menos uno: {aprobados_al_menos_1}")
 
 
+'''
 8) Armá un diccionario donde las claves sean nombres de productos y los valores su stock.
 Permití al usuario:
 • Consultar el stock de un producto ingresado.
@@ -138,8 +149,8 @@ stock_productos = {
     "azucar": 6
 }
 
-productos_ingresado = stock_productos.keys()
-print(f"El esto de productos es {productos_ingresado}")
+productos_ingresado = stock_productos
+print(f"El Stock de productos: {productos_ingresado}")
 
 producto_agregar = input("Indique un productos para agregar unidades ").lower()
 
@@ -152,3 +163,58 @@ else:
     cantidad = int(input("¿Cuántas unidades tiene el nuevo producto?: "))
     stock_productos[producto_agregar] = cantidad
     print(f"Producto '{producto_agregar}' agregado con {cantidad} unidades.")
+    print(f"Este es el Stock actualizado: {productos_ingresado}")
+
+
+'''
+9) Creá una agenda donde las claves sean tuplas de (día, hora) y los valores sean eventos.
+'''
+agenda_clave_valor = {
+    ("Lunes", "09:00"): "Reunión con el equipo",
+    ("Lunes", "14:00"): "Llamada con proveedor",
+    ("Martes", "10:30"): "Presentación del proyecto",
+    ("Miércoles", "16:00"): "Clase de inglés",
+    ("Jueves", "13:00"): "Almuerzo con cliente",
+    ("Viernes", "18:00"): "Cierre de semana y planificación"
+}
+
+#Función Consulta
+def consulta_actividad(dia, hora):
+    clave = (dia, hora)
+    if clave in agenda_clave_valor:
+        print(f"{dia} a las {hora}: {agenda_clave_valor[clave]}")
+    else:
+        print(f"No hay actividades para el día {dia} a las {hora}h.")
+
+consulta_dia = input("Ingrese el día que desea consultar en la agenda:")
+consulta_hora = input("Ingrese la hora que sea consultar:")
+consulta_actividad(consulta_dia, consulta_hora)
+
+
+'''
+10) Dado un diccionario que mapea nombres de países con sus capitales, construí un nuevo
+diccionario donde:
+• Las capitales sean las claves.
+• Los países sean los valores.
+'''
+paises_capitales_sudamerica = {
+    "Argentina": "Buenos Aires",
+    "Bolivia": "Sucre",
+    "Brasil": "Brasilia",
+    "Chile": "Santiago",
+    "Colombia": "Bogotá",
+    "Ecuador": "Quito",
+    "Guyana": "Georgetown",
+    "Paraguay": "Asunción",
+    "Perú": "Lima",
+    "Surinam": "Paramaribo",
+    "Uruguay": "Montevideo",
+    "Venezuela": "Caracas"
+}
+
+capitales_paises_sudamerica = {}
+
+for pais, capital in paises_capitales_sudamerica.items():
+    capitales_paises_sudamerica[capital] = pais
+print(f"Listado paises  y capitales {paises_capitales_sudamerica}")
+print(f"Lista invertidad: {capitales_paises_sudamerica}")
